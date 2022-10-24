18f79296917126c5f0ff3350fe431236a3e193c5
alonso <alonso@gmail.com>
2022-10-24 05:47:48 -0500

    inicio2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const apiRoute = require("./routes/routes");
+const apiRoute = require("./src/routes/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
